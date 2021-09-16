a7a613491d8e901c612b7c00e1daea3a946f6f63
JeffersonKaioSouza <jeffersonkaiosouza@hotmail.com>
2021-09-16 16:52:49 -0300

    jogo-forca

---
+++ forca.py
@@ -0,0 +1,39 @@
+'''
+Jogo da forca
+'''
+
+secreto = 'perfume'
+digitadas = []
+print('=' * 20)
+print('Usado por homens e mulheres, existem caros e baratos,\n'
+      'alguns são doces outros nem tanto, alguns são cheirosos outros não!!\n'
+      'Sabe oque é ?')
+print('=' * 20)
+
+while True:
+    letra = input('Digite uma letra: ').strip().lower()
+
+    if len(letra) > 1:
+        print('Ahhh isso não vale, digite apenas uma letra.')
+        continue
+
+    digitadas.append(letra)
+
+    if letra in secreto:
+        print(f'UHULLL, a letra "{letra}" existe na palavra secreta.')
+    else:
+        print(f'AAHHH,a letra "{letra}" Não existe na palavra secreta.')
+        digitadas.pop()
+
+    secreto_temporario = ''
+    for letra_secreta in secreto:
+        if letra_secreta in digitadas:
+            secreto_temporario += letra_secreta
+        else:
+            secreto_temporario += '*'
+
+    if secreto_temporario == secreto:
+        print(f'Que legal você ganhou!!! A palavra era {secreto_temporario}')
+        break
+    else:
+        print(f'A palavra secreta está assim: {secreto_temporario}')
